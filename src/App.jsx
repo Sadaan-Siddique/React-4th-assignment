59e0163b823b2745d0b618fb2e27400fd4d51e18
Sadaan-Siddique <muhammadsadaan20@gmail.com>
2023-01-22 13:09:58 +0500

    Boot strap Lagai Hai

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import './App.css';
-
+import { Routes, Route,} from "react-router-dom";
+import Nav from './Components/Nav';
+import Main from './Components/Main';
+import Footer from './Components/Footer';
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path='/' element={<Nav/>}/>
+        <Route path='main' element={<Main/>}/>
+        <Route path='footer' element={<Footer/>}/>
+      </Routes>
     </div>
   );
 }
