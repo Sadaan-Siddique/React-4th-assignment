e37d6ccf948a8231ca4bb9e95c478581aab4999e
Sadaan-Siddique <muhammadsadaan20@gmail.com>
2023-01-24 22:46:23 +0500

    I remove main div's

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -6,13 +6,8 @@ function Main() {
     return (
         <>
             <div>
-                <div className="div1 row">
-                    <div className='div2 col-lg-6'>
-                        There are some things or special things that if you will see them ,I am sure you would also like them 
-                    </div>
-                    <div className='col-lg-6'>
-                        Some Text   
-                    </div>
+                <div className="div1 ">
+                    
                 </div>
             </div>
         </>
