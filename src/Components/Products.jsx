32f164779a2f355d530c5c4d05b3300041cb308e
Sadaan-Siddique <muhammadsadaan20@gmail.com>
2023-01-26 00:01:10 +0500

    Card ready kr rha hn

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
-
+import ProductsChild from './ProductsChild'
 function Products() {
+    let arr = [
+        {
+            img: 'https://content.api.news/v3/images/bin/b06bef9a5f8153a24f4abb07cbc2c11e?width=650',
+            title: 'Product 1'
+        },
+        {
+            img: 'https://content.api.news/v3/images/bin/b06bef9a5f8153a24f4abb07cbc2c11e?width=650',
+            title: 'Product 1'
+        },
+        {
+            img: 'https://content.api.news/v3/images/bin/b06bef9a5f8153a24f4abb07cbc2c11e?width=650',
+            title: 'Product 1'
+        }
+    ]
+    let newArr = arr.map((item) => {
+        return <ProductsChild key={item} data={item} />
+    })
     return (
         <>
-        <div>
-            Products
-        </div>
+            <div>
+                <p className='h2 text-center'>Here are some Products :</p>
+                <p className='h4 mt-5 text-center'>What do you Want to Purchase?</p>
+                <div className="card" style={{ "width": "18rem" }}>
+                    {newArr}
+                </div>
+            </div>
+
         </>
     )
 }
---
+++ src/Components/ProductsChild.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Outlet, Link } from "react-router-dom";
+function ProductsChild(props) {
+    return (
+        <>
+                    <img src={props.data.img} className="card-img-top" alt="img"/>
+                    <div className="card-body">
+                        <h5 className="card-title">{props.data.title}</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                    </div>
+        </>
+    )
+}
+
+export default ProductsChild
