387710ac435dd861d6badb63a9772f55c30368ba
Sadaan-Siddique <muhammadsadaan20@gmail.com>
2023-01-29 00:00:38 +0500

    Link kaam krrhe hain

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,26 +1,10 @@
 import React from 'react'
-import MotorCycle from './Cards/MotorCycle'
-import Books from './Cards/Books'
-import Cameras from './Cards/Cameras'
-import Car from './Cards/Car'
-import Jackets from './Cards/Jackets'
-import Shirt from './Cards/Shirt'
-import Laptops from './Cards/Laptops'
-import Watches from './Cards/Watches'
-import Helmets from './Cards/Helmets'
+import { Outlet } from 'react-router-dom'
 function Cards() {
     return (
         <>
-        <MotorCycle/>
-        <Books/>
-        <Cameras/>
-        <Jackets/>
-        <Laptops/>
-        <Car/>
-        <Helmets/>
-        <Shirt/>
-        <Watches/>
-        </>
+        <Outlet/>
+       </>
     )
 }
 
--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -9,15 +9,15 @@ function Products() {
             </div>
             <div className='row text-center justify-content-between border border-2 border-primary p-1'>
                 <div className='col-lg-2 border border-2 border-danger ms-5 p-3'>
-                    <Link className="btn btn-outline-success d-block " to="services">MotorCycle</Link>
-                    <Link className="btn btn-outline-success d-block " to="services">Car</Link>
-                    <Link className="btn btn-outline-success d-block " to="services">Laptops</Link>
-                    <Link className="btn btn-outline-success d-block " to="services">Cameras</Link>
+                    <Link className="btn btn-outline-success d-block " to="cards/motorcycle">MotorCycle</Link>
+                    <Link className="btn btn-outline-success d-block " to="cards/car">Car</Link>
+                    <Link className="btn btn-outline-success d-block " to="cards/laptops">Laptops</Link>
+                    <Link className="btn btn-outline-success d-block " to="cards/cameras">Cameras</Link>
                     <Link className="btn btn-outline-success d-block " to="cards/books">Books</Link>
-                    <Link className="btn btn-outline-success d-block " to="services">Watches</Link>
-                    <Link className="btn btn-outline-success d-block " to="services">Shirt</Link>
-                    <Link className="btn btn-outline-success d-block " to="services">Helmets</Link>
-                    <Link className="btn btn-outline-success d-block " to="services">Jackets</Link>
+                    <Link className="btn btn-outline-success d-block " to="cards/watches">Watches</Link>
+                    <Link className="btn btn-outline-success d-block " to="cards/shirt">Shirt</Link>
+                    <Link className="btn btn-outline-success d-block " to="cards/helmets">Helmets</Link>
+                    <Link className="btn btn-outline-success d-block " to="cards/jackets">Jackets</Link>
                 </div>
                 <div className='col-lg-8 border border-2 border-info me-5 p-3'>
                     <div className=''>
