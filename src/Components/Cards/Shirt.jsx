f074cac42b4bb7c091c682a0ce145fabdfb883b4
Sadaan-Siddique <muhammadsadaan20@gmail.com>
2023-01-29 00:12:16 +0500

    Product lagaye hein

--- src/Components/Cards/Shirt.jsx
+++ src/Components/Cards/Shirt.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import img from '../images/Background.jpg'
 
 function Shirt() {
-  return (
-    <div>Shirt</div>
-  )
+    return (
+        <>
+            <div>Shirt</div>
+            <div>Books</div>
+            <div className="card" style={{ "width": "18rem", border: '2px solid blue' }}>
+                <img src={img} className="card-img-top " alt="img" />
+                <div className="card-body">
+                    <h5 className="card-title">Book</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <Link to='/' className="btn btn-primary">Go somewhere</Link>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Shirt
--- src/Components/CardsChild.jsx
+++ src/Components/CardsChild.jsx
@@ -3,14 +3,7 @@ import { Link } from "react-router-dom";
 function CardsChild(props) {
     return (
         <>
-            <div className="card" style={{ "width": "18rem" , border:'2px solid blue' }}>
-                <img src={props.data.img} className="card-img-top " alt="img" />
-                <div className="card-body">
-                    <h5 className="card-title">{props.data.title}</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <Link to={props.data.to} className="btn btn-primary">Go somewhere</Link>
-                </div>
-            </div>
+            
 
         </>
     )
