58a7cf6492661d0cf1576cc2ef45433d913907cc
Sadaan-Siddique <muhammadsadaan20@gmail.com>
2023-01-28 16:28:25 +0500

    Links

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,15 @@ import Footer from './Components/Footer';
 import Products from './Components/Products';
 import Services from './Components/Services';
 import Cards from './Components/Cards';
+import MotorCycle from './Components/Cards/MotorCycle';
+import Books from './Components/Cards/Books';
+import Cameras from './Components/Cards/Cameras';
+import Car from './Components/Cards/Car';
+import Helmets from './Components/Cards/Helmets';
+import Jackets from './Components/Cards/Jackets';
+import Laptops from './Components/Cards/Laptops';
+import Shirt from './Components/Cards/Shirt';
+import Watches from './Components/Cards/Watches';
 function App() {
   return (
     <div className="App">
@@ -13,7 +22,17 @@ function App() {
       <Routes>
         <Route path='/' element={<Main />} />
         <Route path='products' element={<Products />}>
-          <Route path='cards' element={<Cards/>} />
+          <Route path='cards' element={<Cards />}>
+            <Route path='motorcycle' element={<MotorCycle />} />
+            <Route path='car' element={<Car />} />
+            <Route path='laptops' element={<Laptops />} />
+            <Route path='cameras' element={<Cameras />} />
+            <Route path='books' element={<Books />} />
+            <Route path='watches' element={<Watches />} />
+            <Route path='shirt' element={<Shirt />} />
+            <Route path='helmets' element={<Helmets />} />
+            <Route path='jackets' element={<Jackets />} />
+          </Route>
         </Route>
         <Route path='services' element={<Services />} />
       </Routes>
